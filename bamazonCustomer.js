063d4c66ad31c54ee018bf6dfe9fbdecee1e7a36
Jarrell Houston <jhouston2882@yahoo.com>
2019-11-30 21:46:20 -0600

    shows products and feature to buy item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,3 +18,40 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 });
+
+showProducts();
+
+function buyItem() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productId",
+            message: "Enter the ID of the product you would like to buy:"
+        },
+        {
+            type: "input",
+            name: "productQuantity",
+            message: "Enter the quantity to buy:"
+        }
+    ]).then(function (userInput) {
+        purchaseProduct(parseInt(userInput.productId), parseInt(userInput.productQuantity));
+    })
+}
+
+
+function showProducts() {
+
+    // console.log("connected as id " + connection.threadId + "\n");
+
+    var query = connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        // console.log(res);
+        console.log("Here are the available items:");
+        res.forEach(item => console.log("Id: " + item.item_id + " | Product: " + item.product_name + " | Price: $" + item.price + " | Quantity: " + item.stock_quantity));
+
+        buyItem();
+
+    })
+
+}
