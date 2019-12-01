2f4eafb038d9ace7c269854316ac9c5fc8a13e1d
Jarrell Houston <jhouston2882@yahoo.com>
2019-11-30 21:51:40 -0600

    shows options for manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -19,3 +19,35 @@ connection.connect(function (err) {
     if (err) throw err;
 });
 
+choices();
+
+function choices() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "options",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
+            message: "What do you want to do?"
+        }
+    ]).then(function (choice) {
+        switch (choice.options) {
+            case "View Products for Sale":
+                showProducts();
+                break;
+            case "View Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addToInventory();
+                break;
+            case "Add New Product":
+                newProduct();
+                break;
+            case "Exit":
+                console.log("Exited Program");
+                process.exit();
+                break;
+
+        }
+    })
+}
