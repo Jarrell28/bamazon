736a219b4f1f6146ebbd345723ba4eedaca53239
Jarrell Houston <jhouston2882@yahoo.com>
2019-11-30 21:45:35 -0600

    setup connection to database

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
